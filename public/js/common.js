71dd5ee684b7a2ac049eab093a448ed1ae34b3d5
adibarra <93070681+adibarra@users.noreply.github.com>
2023-06-12 16:36:04 -0500

    refactor: simplify

--- public/js/common.js
+++ public/js/common.js
@@ -1,41 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    updateThemeMode();
-    $('#header_favicon').bind('click', () => window.location='/');
-    $('#footer_favicon').bind('click', () => window.location='/');
-    $('#theme_toggle')  .bind('click', () => toggleThemeMode());
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'G-HS51DZ5HEM');
+  updateTheme();
+  $('#header_favicon').bind('click', () => window.location='/');
+  $('#footer_favicon').bind('click', () => window.location='/');
+  $('#theme_toggle')  .bind('click', () => toggleDark());
 });
 
 function isElementVisible(element) {
-    return $(element).offset().top <= ($(window).height() + $(window).scrollTop());
+  return $(element).offset().top <= ($(window).height() + $(window).scrollTop());
 }
 
-function getCookie(name) {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        const c = cookies[i].trim().split('=');
-        if (c[0] === name) return decodeURIComponent(c[1]);
-    }
-    return '';
+function isDark() {
+  if (localStorage)
+    return JSON.parse(localStorage.getItem('dark'));
+  return true;
 }
 
-function setCookie(name, value, days=365, path='/', domain='', samesite='strict') {
-    let cookie = `${name}=${encodeURIComponent(value)}`;
-    const expiry = new Date();
-    expiry.setDate(expiry.getDate() + days);
-    document.cookie = `${cookie}; expires=${expiry.toUTCString()}; path=${path}; domain=${domain}; samesite=${samesite}; secure`;
+function toggleDark() {
+  if (localStorage) {
+    localStorage.setItem('dark', JSON.stringify(!isDark()));
+  }
+  updateTheme();
 }
 
-function toggleThemeMode() {
-    setCookie('style', getCookie('style') === 'light' ? 'dark' : 'light');
-    updateThemeMode();
-}
-
-function updateThemeMode() {
-    if (!getCookie('style')) setCookie('style', 'dark');
-    $('body').attr('class', getCookie('style'));
-    $('#theme_toggle').html('<i class="fa '+(getCookie('style') === 'light' ? 'fa-sun-o' : 'fa-moon-o')+'" title="Toggle Theme" aria-hidden="true"></i>');
+function updateTheme() {
+  $('body').attr('class', isDark() ? 'dark' : 'light');
+  $('#theme_toggle').html('<i class="fa '+(isDark() ? 'fa-moon-o' : 'fa-sun-o')+'" title="Toggle Theme" aria-hidden="true"></i>');
 }
